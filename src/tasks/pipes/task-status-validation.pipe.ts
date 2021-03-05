3be339f5d6877f4d3cb05ac1148be276d58e15fa
Matthias Eder <nodeful@gmail.com>
2021-03-05 08:54:09 +0100

    Added custom validation pipe for updating task statuses

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common'
+import { TaskStatus } from '../task.model'
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatusValues = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE
+  ]
+
+  transform(value: any) {
+    value = value.toUpperCase()
+    if (this.isValidStatus(value)) {
+      return value
+    }
+    throw new BadRequestException(`"${value}" is an invalid task status`)
+  }
+
+  private isValidStatus(value: any): boolean {
+    return this.allowedStatusValues.indexOf(value) !== -1
+  }
+}
