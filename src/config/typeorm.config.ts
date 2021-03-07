a64bbc51092bc5c38a9bbfeb70c28797177a6cae
Matthias Eder <nodeful@gmail.com>
2021-03-06 16:11:05 -0800

    Updated TypeORM configuration and README

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'meder',
+  username: 'postgres',
   database: 'taskmanagement',
   autoLoadEntities: true,
   synchronize: true
