441e93939ec43124d22cb7ce74e7d8670b7a2bc6
Matthias Eder <nodeful@gmail.com>
2021-03-10 14:18:05 -0800

    Added default logger

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -25,6 +26,7 @@ import { TasksService } from './tasks.service'
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger: Logger = new Logger('TasksController')
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -32,6 +34,11 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: TaskFilterDto,
     @GetUser() user: User
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User ${user.username} requesting all tasks. Filters ${JSON.stringify(
+        filterDto
+      )}`
+    )
     return this.tasksService.getTasks(filterDto, user)
   }
 
@@ -49,6 +56,11 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(
+      `User ${user.username} creating task. Data: ${JSON.stringify(
+        createTaskDto
+      )}`
+    )
     return this.tasksService.createTask(createTaskDto, user)
   }
 
