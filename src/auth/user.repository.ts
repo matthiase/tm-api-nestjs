430a8319c4cdcbc772df510711f765bfd5714945
Matthias Eder <nodeful@gmail.com>
2021-03-13 16:06:51 -0800

    Replaced bcrypt with bcryptjs

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,7 +2,7 @@ import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { EntityRepository, Repository } from 'typeorm'
 import { CredentialsDto } from './dto/credentials.dto'
 import { User } from './user.entity'
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
